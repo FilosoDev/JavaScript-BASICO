48afb25f80a53a4a6ba58f31ed4ac3f5251d984e
Arthur Ferreira <arthurferreira@usp.br>
2021-08-02 19:07:52 -0300

    Aula-03

---
+++ Aula-03.js
@@ -0,0 +1,27 @@
+﻿//Operadores lógicos
+//igualdade
+//console.log(5 == 3);
+
+//===
+//console.log('5' === 5);
+
+// != diferente
+//console.log(5 != 3);
+//console.log(5 != 5);
+
+//condicionais e operadores lógicos
+// > < <= >= == === != !==
+var media = 4.9;
+
+if (media >= 7) {
+    console.log('Aprovado');
+} else if (media >= 5 && media < 7) {
+    console.log('Recuperação');
+} else {
+    console.log('Reprovado');
+}
+
+// ternário
+console.log(('10' === 10) ? 'Igual' : 'Diferente');
+
+
