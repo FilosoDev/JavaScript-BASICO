603d5290acce2cbb517d4ac23d81342a52a1cad4
Arthur Ferreira <arthurferreira@usp.br>
2021-08-06 21:29:23 -0300

    Aula 04 e 05

---
+++ Aula-04.js
@@ -0,0 +1,32 @@
+﻿//While
+//let contador = 0;
+//while(contador <= 10){
+//    console.log(contador);
+//    contador += 1;
+//}
+
+//do While
+//let contador = 0;
+//do{
+//    console.log(contador);
+//    contador++;
+//}while (contador <= 10);
+
+
+// for
+for (let contador = 0; contador <= 10; contador++) {
+    console.log(contador);
+}
+
+// for no array
+var frutas = ['Maçã', 'Melão', 'Banana', 'Abacate', 'Melancia'];
+
+for(let i = 0; i < frutas.length; i++ ){
+    console.log(frutas[i]);
+}
+
+//foreach
+frutas.forEach(function (elemento, indice, array) {
+    console.log(elemento);
+});
+
---
+++ Aula-05.js
@@ -0,0 +1,15 @@
+﻿var tipoUsuario = "qualquer cargo";
+
+switch (tipoUsuario) {
+    case "Admin":
+        console.log("Bem-vindo Admin");
+        break;
+    case "Gerente":
+        console.log("Bem-vindo gerente");
+        break;
+    case "Secretário-assistente":
+        console.log("Bem-vindo Secretário-assistente");
+        break;
+    default:
+        console.log("Bem-vindo anônimo");
+}
